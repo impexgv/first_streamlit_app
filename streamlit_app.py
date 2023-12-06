d58c7f0f25c613932c1af4782734efbbccdca395
impexgv <152347977+impexgv@users.noreply.github.com>
2023-12-05 22:14:25 -0500

    Update streamlit_app.py
    response

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,4 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruit_to_show = my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruit_to_show)
-
+streamlit.text(fruityvice_response)
